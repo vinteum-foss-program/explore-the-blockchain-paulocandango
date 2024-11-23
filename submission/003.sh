7997b918287fd1a760f959f4fce3229d5fb928ab
Paulo.Moreira <paulocandango@gmail.com>
2024-11-23 17:55:15 -0300

    Descobre o hash do bloco, os dados do bloco e conta a quantidade de saidas

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,5 @@
 # How many new outputs were created by block 123,456?
+hash_do_bloco=$(bitcoin-cli getblockhash 123456)
+dados_do_bloco=$(bitcoin-cli getblock $hash_do_bloco true)
+quantidade_saidas=$(echo "$dados_do_bloco" | jq '[.tx[] | . as $tx | length] | add')
+echo $quantidade_saidas
