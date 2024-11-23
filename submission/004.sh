d4cdbaf04088b22fbbe88a30e4743d33c88a4db0
Paulo.Moreira <paulocandango@gmail.com>
2024-11-23 19:59:04 -0300

    Imprimindo uma chave que eu acho que não é a do teste

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,6 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+descritor=$(bitcoin-cli getdescriptorinfo "tr($xpub/100/*)" | jq -r '.descriptor')
+endereco_taproot=$(bitcoin-cli deriveaddresses "$descritor" 0 | jq -r '.[0]')
+echo "$endereco_taproot"
