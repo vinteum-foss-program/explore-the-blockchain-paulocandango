b0927cc05879c031c43c9eff6de11c43b1e8155b
Paulo.Moreira <paulocandango@gmail.com>
2024-11-23 21:37:42 -0300

    Teste

--- submission/004.sh
+++ submission/004.sh
@@ -1,6 +1,8 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+bitcoin-cli --version
 descritor=$(bitcoin-cli getdescriptorinfo "tr($xpub/100/*)" | jq -r '.descriptor')
+echo "Descritor gerado: $descritor"
 endereco_taproot=$(bitcoin-cli deriveaddresses "$descritor" 0 | jq -r '.[0]')
-echo "$endereco_taproot"
+echo "Endereço derivado: $endereco_taproot"
